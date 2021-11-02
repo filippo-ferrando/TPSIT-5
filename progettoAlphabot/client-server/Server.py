644b8a957f2c7ee7783b349886d50d0416c831e8
filippo-ferrando <filippo.ferrando@itiscuneo.eu>
2021-11-02 17:05:51 +0100

    new file:   appuntiTPSIT.txt 	new file:   progettoAlphabot/README.md 	new file:   progettoAlphabot/client-server/Client.py 	new file:   progettoAlphabot/client-server/Server.py

---
+++ progettoAlphabot/client-server/Server.py
@@ -0,0 +1,138 @@
+'''
+Author: Michele Alladio
+es:'''
+
+import threading as thr
+import socket as sck
+
+#class AlphaBot(object):
+class AlphaBot(thr.Thread):
+    def __init__(self, s):
+        thr.Thread.__init__(self)   #costruttore super (java) 
+        self.running = True
+        self.s = s
+    
+    def __init__(self, in1=13, in2=12, ena=6, in3=21, in4=20, enb=26):
+        self.IN1 = in1
+        self.IN2 = in2
+        self.IN3 = in3
+        self.IN4 = in4
+        self.ENA = ena
+        self.ENB = enb
+        self.PA  = 50
+        self.PB  = 50
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setwarnings(False)
+        GPIO.setup(self.IN1, GPIO.OUT)
+        GPIO.setup(self.IN2, GPIO.OUT)
+        GPIO.setup(self.IN3, GPIO.OUT)
+        GPIO.setup(self.IN4, GPIO.OUT)
+        GPIO.setup(self.ENA, GPIO.OUT)
+        GPIO.setup(self.ENB, GPIO.OUT)
+        self.PWMA = GPIO.PWM(self.ENA,500)
+        self.PWMB = GPIO.PWM(self.ENB,500)
+        self.PWMA.start(self.PA)
+        self.PWMB.start(self.PB)
+        self.stop()'''
+
+    def forward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)'''
+        print(f'AVANTI'.encode())
+
+    def stop(self):
+        '''self.PWMA.ChangeDutyCycle(0)
+        self.PWMB.ChangeDutyCycle(0)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.LOW)'''
+        print(f'STOP'.encode())
+
+    def backward(self):
+        self.PWMA.ChangeDutyCycle(self.PA)
+        self.PWMB.ChangeDutyCycle(self.PB)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)'''
+        print(f'INDIETRO'.encode())
+
+    def left(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.LOW)
+        GPIO.output(self.IN2, GPIO.HIGH)
+        GPIO.output(self.IN3, GPIO.HIGH)
+        GPIO.output(self.IN4, GPIO.LOW)'''
+        print(f'SINISTRA'.encode())
+
+    def right(self, speed=30):
+        self.PWMA.ChangeDutyCycle(speed)
+        self.PWMB.ChangeDutyCycle(speed)
+        GPIO.output(self.IN1, GPIO.HIGH)
+        GPIO.output(self.IN2, GPIO.LOW)
+        GPIO.output(self.IN3, GPIO.LOW)
+        GPIO.output(self.IN4, GPIO.HIGH)'''
+        print(f'DESTRA'.encode())
+        
+    def set_pwm_a(self, value):
+        self.PA = value
+        self.PWMA.ChangeDutyCycle(self.PA)
+
+    def set_pwm_b(self, value):
+        self.PB = value
+        self.PWMB.ChangeDutyCycle(self.PB)    
+        
+    def set_motor(self, left, right):
+        if (right >= 0) and (right <= 100):
+            GPIO.output(self.IN1, GPIO.HIGH)
+            GPIO.output(self.IN2, GPIO.LOW)
+            self.PWMA.ChangeDutyCycle(right)
+        elif (right < 0) and (right >= -100):
+            GPIO.output(self.IN1, GPIO.LOW)
+            GPIO.output(self.IN2, GPIO.HIGH)
+            self.PWMA.ChangeDutyCycle(0 - right)
+        if (left >= 0) and (left <= 100):
+            GPIO.output(self.IN3, GPIO.HIGH)
+            GPIO.output(self.IN4, GPIO.LOW)
+            self.PWMB.ChangeDutyCycle(left)
+        elif (left < 0) and (left >= -100):
+            GPIO.output(self.IN3, GPIO.LOW)
+            GPIO.output(self.IN4, GPIO.HIGH)
+            self.PWMB.ChangeDutyCycle(0 - left)'''
+
+def main():
+    s = sck.socket(sck.AF_INET, sck.SOCK_STREAM) 
+    s.bind(('localhost', 7000)) 
+
+    Ab = AlphaBot(s)
+    Ab.start()
+
+    s.listen()
+    conn, addr = s.accept()
+    print(f"Connessione avvenuta con: {addr}")
+
+    while True:
+        data = conn.recv(4096).decode()   
+
+        if data == 'W':
+            Ab.forward()
+        elif data == 'S':
+            Ab.backward()
+        elif data == 'D':
+            Ab.right()
+        elif data == 'A':
+            Ab.left()
+        elif data == 'STOP':
+            Ab.stop() 
+
+    s.close()    
+    
+if __name__ == "__main__":
+    main()
